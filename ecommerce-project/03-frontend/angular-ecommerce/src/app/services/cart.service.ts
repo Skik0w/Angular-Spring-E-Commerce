7ff86881445c667e139be27b62251f49a37c8c34
Skik0w <bartekkow02@gmail.com>
2025-03-27 00:30:29 +0100

    add

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -49,7 +49,7 @@ export class CartService {
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
 
-    // log cart data just for debbuging purposes
+    // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
   }
 
@@ -67,5 +67,31 @@ export class CartService {
       )}, totalQuantity: ${totalQuantityValue}`
     );
     console.log(`----`);
+  }
+
+
+  decrementQuantity(theCartItem: CartItem) {
+
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
   }
 }

28003cada13c06b5156f01a80fe4b362323ab5e6
Skik0w <bartekkow02@gmail.com>
2025-03-27 20:41:04 +0100

    add

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CartItem } from '../../common/cart-item';
 import { CartService } from '../../services/cart.service';
 
@@ -6,33 +6,29 @@ import { CartService } from '../../services/cart.service';
   selector: 'app-cart-details',
   standalone: false,
   templateUrl: './cart-details.component.html',
-  styleUrls: ['./cart-details.component.css']
+  styleUrl: './cart-details.component.css',
 })
-export class CartDetailsComponent implements OnInit {
-
+export class CartDetailsComponent {
   cartItems: CartItem[] = [];
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.listCartDetails();
   }
 
   listCartDetails() {
-
     // get a handle to the cart items
     this.cartItems = this.cartService.cartItems;
 
     // subscribe to the cart totalPrice
-    this.cartService.totalPrice.subscribe(
-        data => this.totalPrice = data
-    );
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
 
     // subscribe to the cart totalQuantity
     this.cartService.totalQuantity.subscribe(
-        data => this.totalQuantity = data
+      (data) => (this.totalQuantity = data)
     );
 
     // compute cart total price and quantity

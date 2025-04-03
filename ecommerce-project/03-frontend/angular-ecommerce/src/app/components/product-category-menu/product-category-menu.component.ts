ab92ff75a122fa0c13987a2d5c86e4ae8dba86af
Skik0w <bartekkow02@gmail.com>
2025-04-03 19:40:34 +0200

    add

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -24,7 +24,7 @@ export class ProductCategoryMenuComponent implements OnInit{
   listProductCategories() {
     this.productService.getProductCategories().subscribe(
       data => {
-        console.log('Product Categrories=' + JSON.stringify(data));
+        console.log('Product Categories=' + JSON.stringify(data));
         this.productCategories = data;
       }
     )

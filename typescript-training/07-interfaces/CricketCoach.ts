d0ce7efe54e72727442f2552567409744794d69c
Skik0w <bartekkow02@gmail.com>
2025-04-04 21:30:23 +0200

    add

--- typescript-training/07-interfaces/CricketCoach.ts
+++
@@ -1,9 +0,0 @@
-import { Coach } from "./Coach"
-
-export class CricketCoach implements Coach{
-
-    getDailyWorkout(): string {
-        return "Practive your spin bowling technique."
-    }
-    
-}

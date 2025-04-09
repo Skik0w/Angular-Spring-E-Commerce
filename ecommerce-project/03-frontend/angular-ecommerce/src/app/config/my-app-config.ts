58a76f951edeb3ba4951e20c43c2f42e510721a1
Skik0w <bartekkow02@gmail.com>
2025-04-09 23:50:13 +0200

    add

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+    clientId: '0oao7xvv9cRLWudIM5d7',
+    issuer: 'https://dev-34572226.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  }
+
+}
